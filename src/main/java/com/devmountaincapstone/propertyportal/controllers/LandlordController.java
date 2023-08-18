5042f8ac5cd91b4735b52772156a61c8f995783b
Myron Holmes <1dequanh@gmail.com>
2023-08-18 18:09:36 -0400

    html, css and javascript files created

--- src/main/java/com/devmountaincapstone/propertyportal/controllers/LandlordController.java
+++ src/main/java/com/devmountaincapstone/propertyportal/controllers/LandlordController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/dev/envi/landlords")
+@RequestMapping("/landlords")
 public class LandlordController {
     @Autowired
     private LandlordService landlordService;
